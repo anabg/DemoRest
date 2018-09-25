0e9b06054d8faa09e59b2e54e4a4b6db5453708d
Grimaut, Ana <Ana_Grimaut@McAfee.com>
2018-09-25 15:47:20 -0300

    add modification in PersonaController

--- src/main/java/com/example/demo/controller/PersonaController.java
+++ src/main/java/com/example/demo/controller/PersonaController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.ws.rs.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,18 +34,24 @@ public class PersonaController {
 		return personaService.listar();
 	}
 	
+	
+	@GetMapping(value="/listar/{id}")
+	public Persona listar(@PathVariable("id") int id){
+		return personaService.listarId(id);
+	}
+	
 	@PostMapping(value="/registrar", consumes=MediaType.APPLICATION_JSON_VALUE)
 	public Persona registrar(@RequestBody Persona persona) {
 		return personaService.registrar(persona);
 	}
 	
-	@PutMapping(value="/modificar")
+	@PutMapping(value="/modificar", consumes=MediaType.APPLICATION_JSON_VALUE)
 	public Persona modificar(Persona persona) {
 		return personaService.modificar(persona);
 	}
 	
-	@DeleteMapping(value="/eliminar")
-	public int eliminar(int id) {
+	@DeleteMapping(value="/eliminar/{id}")
+	public int eliminar(@PathVariable("id") int id) {
 		return personaService.eliminar(id);
 	}
 	
